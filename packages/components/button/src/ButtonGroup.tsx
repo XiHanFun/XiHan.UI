ec528dbea42af1ea56d0b161c2f77867daab90bd
zhaifanhua <zhaifanhua@foxmail.com>
2025-02-06 22:45:36 +0800

    Button 组件

---
+++ packages/components/button/src/ButtonGroup.tsx
@@ -0,0 +1,31 @@
+import { defineComponent, provide, h } from "vue";
+import type { InjectionKey } from "vue";
+
+export const buttonGroupContextKey: InjectionKey<{ size?: string }> = Symbol(
+  "buttonGroupContextKey"
+);
+
+export default defineComponent({
+  name: "XhButtonGroup",
+
+  props: {
+    // 统一设置按钮组内按钮的尺寸
+    size: {
+      type: String,
+      values: ["small", "medium", "large"],
+    },
+  },
+
+  setup(props, { slots }) {
+    // 通过 provide/inject 向子按钮提供尺寸信息
+    provide(buttonGroupContextKey, {
+      size: props.size,
+    });
+
+    return () => (
+      <div class="xh-button-group" role="group">
+        {slots.default?.()}
+      </div>
+    );
+  },
+});
