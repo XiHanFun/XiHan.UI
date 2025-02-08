8cf3e088f76bb0d6bfd7a4ce052ed8bb71292c2a
zhaifanhua <zhaifanhua@foxmail.com>
2025-02-09 06:04:57 +0800

    feat: 优化按钮功能

--- packages/components/button/src/ButtonGroup.tsx
+++ packages/components/button/src/ButtonGroup.tsx
@@ -1,7 +1,16 @@
 import { defineComponent, provide } from "vue";
-import type { InjectionKey } from "vue";
+import type { InjectionKey, PropType } from "vue";
+import type { ButtonSize, ButtonType } from "./Button";
 
-export const buttonGroupContextKey: InjectionKey<{ size?: string }> = Symbol("buttonGroupContextKey");
+export interface ButtonGroupContext {
+  size?: ButtonSize;
+  type?: ButtonType;
+  vertical?: boolean;
+  round?: boolean;
+  plain?: boolean;
+}
+
+export const buttonGroupContextKey: InjectionKey<ButtonGroupContext> = Symbol("buttonGroupContextKey");
 
 export default defineComponent({
   name: "XhButtonGroup" as const,
@@ -9,19 +18,43 @@ export default defineComponent({
   props: {
     // 统一设置按钮组内按钮的尺寸
     size: {
-      type: String,
-      values: ["small", "medium", "large"],
+      type: String as PropType<ButtonSize>,
+      default: "medium",
     },
+    // 统一设置按钮组内按钮的类型
+    type: {
+      type: String as PropType<ButtonType>,
+      default: "default",
+    },
+    // 是否为垂直按钮组
+    vertical: Boolean,
+    // 是否为圆角按钮组
+    round: Boolean,
+    // 是否为朴素按钮组
+    plain: Boolean,
   },
 
   setup(props, { slots }) {
-    // 通过 provide/inject 向子按钮提供尺寸信息
+    // 通过 provide/inject 向子按钮提供信息
     provide(buttonGroupContextKey, {
       size: props.size,
+      type: props.type,
+      vertical: props.vertical,
+      round: props.round,
+      plain: props.plain,
     });
 
     return () => (
-      <div class="xh-button-group" role="group">
+      <div
+        class={[
+          "xh-button-group",
+          {
+            "is-vertical": props.vertical,
+            "is-round": props.round,
+          },
+        ]}
+        role="group"
+      >
         {slots.default?.()}
       </div>
     );
