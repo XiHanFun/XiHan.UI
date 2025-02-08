8480af8a6ba65847d6b9402c8a7122d87d376cc4
zhaifanhua <zhaifanhua@foxmail.com>
2025-02-09 07:00:57 +0800

    test: 测试图标

--- playground/main.ts
+++ playground/main.ts
@@ -3,7 +3,9 @@ import XihanUI from "xihan-ui";
 import App from "./App.vue";
 import "@xihan-ui/themes/src/index.scss";
 import "@xihan-ui/components/button/styles/index";
+import { iconManager } from "@xihan-ui/themes";
 
 const app = createApp(App);
 app.use(XihanUI);
+app.use(iconManager);
 app.mount("#app");
