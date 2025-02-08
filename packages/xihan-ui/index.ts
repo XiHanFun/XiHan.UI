1091a07d1231ab631cb7e218169663298c14d120
zhaifanhua <zhaifanhua@foxmail.com>
2025-02-09 06:05:29 +0800

    feat: 添加总包

---
+++ packages/xihan-ui/index.ts
@@ -0,0 +1,21 @@
+// 导出所有子包
+export * from "@xihan-ui/utils";
+export * from "@xihan-ui/constants";
+export * from "@xihan-ui/directives";
+export * from "@xihan-ui/hooks";
+export * from "@xihan-ui/locales";
+export * from "@xihan-ui/themes";
+export * from "@xihan-ui/components";
+
+// 导出 Vue 插件安装函数
+import type { App } from "vue";
+import { install as installComponents } from "@xihan-ui/components";
+
+export const install = (app: App) => {
+  installComponents(app);
+  // 其他子包的安装逻辑
+};
+
+// 导出默认对象
+const XihanUI = { install };
+export default XihanUI;
