621afb374dab5acd384eddcd50192208564e8a3c
zhaifanhua <zhaifanhua@foxmail.com>
2025-03-03 08:06:40 +0800

    feat: 优化

--- ui/packages/utils/src/css/index.ts
+++ ui/packages/utils/src/css/index.ts
@@ -1,12 +1,3 @@
 export * from "./bem";
-// export * from "./breakpoints";
-// export * from "./colors";
-// export * from "./flex";
-// export * from "./grid";
-// export * from "./layout";
-// export * from "./media";
 export * from "./responsive";
-// export * from "./sizing";
-// export * from "./spacing";
-// export * from "./typography";
-// export * from "./variables";
+export * from "./theme";
--- ui/play-ground/src/main.ts
+++ ui/play-ground/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import XihanUI from "xihan-ui";
-import "@xihan-ui/themes/index.scss";
 
 const app = createApp(App);
 app.use(XihanUI);
