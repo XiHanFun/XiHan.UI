4fa743794b45c086588790854ac51de621aea4b7
afandawork <afandawork@gmail.com>
2025-03-07 20:50:12 +0530

    feat: 修改文件夹

--- ui/scripts/gen-version.mjs
+++ ui/scripts/gen-version.mjs
@@ -19,10 +19,17 @@ packages.forEach(pkg => {
   }
 });
 
-// 修改 xihan-ui/package.json 的 version
-const xihanUIPkgPath = path.resolve(__dirname, "..", "xihan-ui", "package.json");
-const xihanUIPkgJson = JSON.parse(fs.readFileSync(xihanUIPkgPath, "utf-8"));
-xihanUIPkgJson.version = version;
-fs.writeFileSync(xihanUIPkgPath, JSON.stringify(xihanUIPkgJson, null, 2) + "\n");
+// 修改 internal 下所有 package.json 的 version
+const internalDir = path.resolve(__dirname, "..", "internal");
+const internal = fs.readdirSync(internalDir).filter(pkg => fs.statSync(path.join(internalDir, pkg)).isDirectory());
+
+internal.forEach(pkg => {
+  const pkgPath = path.join(internalDir, pkg, "package.json");
+  if (fs.existsSync(pkgPath)) {
+    const pkgJson = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
+    pkgJson.version = version;
+    fs.writeFileSync(pkgPath, JSON.stringify(pkgJson, null, 2) + "\n");
+  }
+});
 
 console.log(`✨ Updated version for all packages to: ${version}`);
