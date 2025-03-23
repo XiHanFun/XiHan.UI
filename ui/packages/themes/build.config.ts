32bc3ad5b7b729d55018d2b7b87d91f2d554014d
zhaifanhua <zhaifanhua@foxmail.com>
2025-03-24 06:44:38 +0800

    feat: 优化

--- ui/packages/themes/build.config.ts
+++ ui/packages/themes/build.config.ts
@@ -1,21 +1,24 @@
 import { defineBuildConfig } from "unbuild";
-import { resolve } from "path";
-import { execSync } from "child_process";
+import scss from "rollup-plugin-scss";
+
 export default defineBuildConfig({
-  entries: [{ input: "src/index" }],
+  entries: ["src/index"],
   declaration: true,
   clean: true,
-  failOnWarn: false,
   rollup: {
-    inlineDependencies: true, // 内联依赖
-    preserveDynamicImports: true, // 保留动态导入
+    emitCJS: true,
   },
   hooks: {
-    "build:done": ctx => {
-      const srcFile = resolve(__dirname, "src/index.scss");
-      const outFile = resolve(ctx.options.outDir, "styles", "index.css");
-      execSync(`npx sass ${srcFile}:${outFile} --style=compressed`, { stdio: "inherit" });
-      console.log("SCSS 编译完成!");
+    "rollup:options"(ctx, options) {
+      options.plugins = [
+        ...(options.plugins || []),
+        scss({
+          fileName: "index.css",
+          outputStyle: "compressed",
+          sourceMap: true,
+          watch: ctx.options.stub ? "**/*.scss" : undefined,
+        }),
+      ];
     },
   },
 });
--- ui/packages/utils/src/dom/index.ts
+++ ui/packages/utils/src/dom/index.ts
@@ -3,17 +3,6 @@ export { type BEM } from "./bem";
 export { mediaQuery, container, responsive } from "./responsive";
 export { type Breakpoint } from "./responsive";
 
-export {
-  ThemeStyle,
-  ThemeMode,
-  getSystemTheme,
-  watchSystemTheme,
-  flattenThemeVars,
-  generateCSSVars,
-  createThemeManager,
-  CSS_VAR_PREFIX,
-} from "./theme";
-
 export { hexToRgba, rgbaToHex } from "./color";
 
 export { style, cssVar } from "./css";
