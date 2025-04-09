a093314ac01be92f047fdd1930d738aabfc33506
afandawork <afandawork@gmail.com>
2025-04-09 20:57:02 +0530

    优化

---
+++ ui/internal/build/src/constants/pkg.ts
@@ -0,0 +1,5 @@
+export const PKG_PREFIX = "@xihan-ui";
+export const PKG_NAME = "xihan-ui";
+export const PKG_CAMELCASE_NAME = "XihanUi";
+export const PKG_CAMELCASE_LOCAL_NAME = "XihanUiLocale";
+export const PKG_BRAND_NAME = "Xihan UI";
