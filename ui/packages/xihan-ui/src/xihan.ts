94e3d96cbbef722049b942b8ce5eebba61d9dc73
zhaifanhua <zhaifanhua@foxmail.com>
2025-05-23 05:23:52 +0800

    style: 优化

--- ui/packages/xihan-ui/src/xihan.ts
+++ ui/packages/xihan-ui/src/xihan.ts
@@ -4,21 +4,71 @@ import { version } from "../package.json";
  * 曦寒项目信息
  */
 export class XiHan {
-  static XiHanInfo = `
+  /**
+   * 曦寒Logo
+   */
+  static Logo = `
    _  __ ______  _____    _   __
   | |/ //  _/ / / /   |  / | / /
   |   / / // /_/ / /| | /  |/ /
  /   |_/ // __  / ___ |/ /|  /
-/_/|_/___/_/ /_/_/  |_/_/ |_/
-${version}
-Copyright (C)2021-Present ZhaiFanhua All Rights Reserved.
-https://docs.xihanfun.com
-https://github.com/XiHanFun
-https://github.com/XiHanFun/XiHan.UI
+/_/|_/___/_/ /_/_/  |_/_/ |_/`;
 
+  static Version = version;
+
+  /**
+   * 曦寒版权
+   */
+  static Copyright = `Copyright (C)2021-Present ZhaiFanhua All Rights Reserved.`;
+
+  /**
+   * 曦寒组织
+   */
+  static Org = `https://github.com/XiHanFun`;
+
+  /**
+   * 曦寒仓库
+   */
+  static Rep = `https://github.com/XiHanFun/XiHan.UI`;
+
+  /**
+   * 曦寒文档
+   */
+  static Doc = `https://docs.xihanfun.com`;
+
+  /**
+   * 曦寒寄语
+   */
+  static SendWord = `
 碧落降恩承淑颜，共挚崎缘挽曦寒。
 迁般故事终成忆，谨此葳蕤换思短。
-—— 致她
+              —— 致她`;
 
-快速、轻量、高效、用心的开发框架和组件库。基于 DotNet 和 Vue 构建。`;
+  /**
+   * 曦寒标语
+   */
+  static Tagline = `快速、轻量、高效、用心的开发框架和组件库。基于 DotNet 和 Vue 构建。`;
+
+  /**
+   * 入口程序版本
+   */
+  static EntryAssemblyVersion = ``;
+
+  /**
+   * 曦寒
+   */
+  static SayHello() {
+    return `
+欢迎使用曦寒组件
+${this.Logo}
+${this.Version}
+${this.Copyright}
+${this.Org}
+${this.Rep}
+${this.Doc}
+${this.SendWord}
+
+${this.Tagline}
+`;
+  }
 }
