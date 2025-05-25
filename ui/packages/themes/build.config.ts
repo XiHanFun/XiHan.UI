59c3f9fb4df0aff9deaf034a6884cd8648c1e9ff
zhaifanhua <zhaifanhua@foxmail.com>
2025-05-26 07:22:40 +0800

    feat: 升级版本

--- ui/packages/themes/build.config.ts
+++ ui/packages/themes/build.config.ts
@@ -1,24 +1,11 @@
 import { defineBuildConfig } from "unbuild";
-import scss from "rollup-plugin-scss";
 
 export default defineBuildConfig({
   entries: ["src/index"],
   declaration: true,
   clean: true,
+  failOnWarn: false,
   rollup: {
     emitCJS: true,
-  },
-  hooks: {
-    "rollup:options"(ctx, options) {
-      options.plugins = [
-        ...(options.plugins || []),
-        scss({
-          fileName: "index.css",
-          outputStyle: "compressed",
-          sourceMap: true,
-          watch: ctx.options.stub ? "**/*.scss" : undefined,
-        }),
-      ];
-    },
   },
 });
