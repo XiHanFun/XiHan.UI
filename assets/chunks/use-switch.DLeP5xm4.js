b532e34b040ac9c4d3563e3d68838d3656803f64
zhaifanhua <zhaifanhua@foxmail.com>
2026-08-26 13:09:30 +0000

    Deploying to gh-pages from @ XiHanFun/XiHan.UI@03e0048ae8b760f5b5d47af9ec49582e94476b33 🚀

--- assets/chunks/use-switch.DLeP5xm4.js
+++
@@ -1,1 +0,0 @@
-import{v as t}from"./normalize-props.DWkyZsBF.js";import{u as c}from"./use-machine.NTgxVax-.js";import{c as e,s}from"./switch.machine.CbWhRvOp.js";import{h as a}from"./framework.CINlqUGR.js";function h(o,i){const r=c(s,()=>({...o,onCheckedChange:i}));return{api:a(()=>e(r,t))}}export{h as u};
